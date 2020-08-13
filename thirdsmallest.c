9f9e955013602613f7793981de6330116059ee0b
Abhiram Pai <abhirampai1999@gmail.com>
2020-08-13 08:53:59 +0530

    Update thirdsmallest.c

--- thirdsmallest.c
+++ thirdsmallest.c
@@ -38,3 +38,41 @@ int main()
     
     return 0;
 }
+/*#include <stdio.h>
+#include <string.h>
+#define MAX 100000 
+int main()
+{
+    int a[10],n,min=MAX,count=0,min1=MAX,min2=MAX;
+    printf("Enter the length of array");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        
+        if(min>a[i])
+        {
+            min2=min1;
+            min1=min;
+            min=a[i];
+        }
+        else if((min1>a[i])&&(a[i]!=min))
+        {
+            min2=min1;
+            min1=a[i];
+        }
+        else if((min2>a[i])&&(a[i]!=min1))
+        {
+            min2=a[i];
+        }
+    
+        
+    }
+    printf("\n\nThird Minimum=%d\n\n",min2);
+    for (int i =0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+    return 0;
+} */
